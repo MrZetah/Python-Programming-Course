b5f4f748c4fe67e71cb502e5768334a79d93621a
MrZetah <154997315+MrZetah@users.noreply.github.com>
2024-04-11 10:40:16 -0500

    Create challenge_problems_dictionaries.py

---
+++ Unit 4/challenge_problems_dictionaries.py
@@ -0,0 +1,24 @@
+age_dict = {'ZACHARY  AESHLIMAN': 22, 'FARTUN  AHMED': 40, 'GRACE  BAUMANN': 28, 'TESFAHUN  BONITTO': 46, 'AXEL  BRUNS': 20, 
+                  'COLTON  CLARK': 22, 'CARSON  CROSBY': 60, 'Riley  DALEN': 18, 'MOLLIE  GAMBLE': 39, 'ADAM  GOTTWALT': 57,
+                  'COLTON  HERN': 10, 'OWEN  HOEPNER': 38, 'Gena  JUREK': 55, 'DANIEL  LINGL': 27, 'LUIS  LOZANO': 28, 
+                  'COLTAN  PAUL': 28, 'JENNA  RIEDER': 56, 'IAN  ROBINSON': 18, 'CHRISTIAN  RODRIGUEZ': 39,
+                  'JOHN  SCHELONKA': 35, 'THADDEUS  ZABOROWSKI': 40}
+
+grades_dict = {'ZACHARY  AESHLIMAN': 84, 'FARTUN  AHMED': 82, 'GRACE  BAUMANN': 100, 'TESFAHUN  BONITTO': 84,
+               'AXEL  BRUNS': 78, 'COLTON  CLARK': 99, 'CARSON  CROSBY': 100, 'Riley  DALEN': 82, 'MOLLIE  GAMBLE': 79,
+               'ADAM  GOTTWALT': 88, 'COLTON  HERN': 89, 'OWEN  HOEPNER': 73, 'Gena  JUREK': 78, 'DANIEL  LINGL': 90,
+               'LUIS  LOZANO': 96, 'COLTAN  PAUL': 100, 'JENNA  RIEDER': 75, 'IAN  ROBINSON': 72, 'CHRISTIAN  RODRIGUEZ': 72,
+               'JOHN  SCHELONKA': 92, 'THADDEUS  ZABOROWSKI': 96}
+
+analyze_this_string = '''Copy this entire chunk into the beginning of your code. Then loop through this string 
+                        using a for loop. For each character, set the current character as the key of the dictionary 
+                        and use setdefault method to set the value to zero initially. Then immediately add one to the 
+                        value that corresponds to that character key. The next time your loop comes across the same 
+                        character, the set default won’t change the value to zero because it already exists, but the 
+                        value will be incremented by one. Print the dictionary at the end of the loop.'''
+
+products_dict = {'eggs': {'price': 2.50, 'quant': 2},
+                'milk': {'price': 3.70, 'quant': 3},
+                'bread': {'price': 3.29, 'quant': 1},
+                'donuts': {'price': 0.79, 'quant': 12},
+                'cereal': {'price': 4.29, 'quant': 4}}
