749e08f076e379972dc74955288edd73f6e9ea89
MrZetah <154997315+MrZetah@users.noreply.github.com>
2024-04-15 16:18:36 -0500

    Create pygame_template.py

---
+++ Unit 8/pygame_template.py
@@ -0,0 +1,51 @@
+# Pygame Template
+
+# 1 - import packages
+import pygame
+from pygame.locals import *
+import sys
+
+#2 - Define constants
+BLACK = (0, 0, 0)
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+FPS = 30
+
+#3 - Initialize the world
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+#4 - Load assets: images, sounds, etc.
+
+#5 - Initialize variables
+
+#6 - Loop forever
+while True:
+
+    #7 Check for and handle events
+    for event in pygame.event.get():
+        #Check to see if user has quit the game
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        #Press the 'q' key as an option to quit
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit()
+        #Check for other 'game' related events (collisions, key presses, mouse clicks, etc.)
+
+
+    #8 - Do any "per frame" actions (move objects, add or remove items)
+
+    #9 - Clear the window
+    window.fill(BLACK)
+
+    #10 - Draw all window elements
+
+    #11 - Update the window
+    pygame.display.update()
+
+    #12 - Set frame rate to slow things down
+    clock.tick(FPS)
