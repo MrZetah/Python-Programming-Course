ef49734c0dc58a989a7a503d20b8cf6bef740a91
MrZetah <154997315+MrZetah@users.noreply.github.com>
2024-05-02 10:39:56 -0500

    Update If, Else Challenge Problems Partial Solutions.py

--- Unit 2/If, Else Challenge Problems Partial Solutions.py
+++ Unit 2/If, Else Challenge Problems Partial Solutions.py
@@ -1,1 +1,93 @@
+# If/Else Challenge Problems - Partial Solutions
+
+#######################################################
+
+# Challenge 1
+number = int(input('Enter a number '))
+
+if number ___________:      # Fill in the blanks
+    print(number, 'is ______')
+else:
+    print(number, 'is ______')
+
+#######################################################
+
+# Challenge 2
+password = '___________'   # Create a password
+guess = input('Enter Password: ')
+
+if _____________:      # Fill in the blanks
+    print('__________')
+____:                  # What word would you put on this line?
+    print('__________')
+
+#######################################################
+
+# Challenge 3
+number =       # Ask for user input, convert to integer
+
+# Fill in the blanks
+if __________:
+    print(number, 'is positive')
+elif __________:
+    print(number, 'is negative')
+_____:
+    print(number, 'is 0')
+
+#######################################################
+
+# Challenge 4
+number = int(input('Enter a Number: '))
+
+# Fill in the blanks
+if number % 3 == 0 and _____________:
+    print(number, 'is a multiple of 3 and 5')
+elif ____________:
+    print(number, 'is a multiple of 3')
+elif _____________:
+    print(number, 'is a multiple of 5')
+else:
+    print(number, 'is not a multiple of 3 and 5')
+
+
+#######################################################
+
+# Challenge 5
+grade = int(input('What is your grade? '))
+
+# Fill in the blanks
+
+if __________:
+    print('You got an A! Great Job!')
+elif __________:
+    print('You got a B, Nice!')
+elif __________:
+    print('You got a C!')
+elif __________:
+    print('You got a D, phew')
+_____:
+    print('You got an F, better study.')
+
+
+
+#######################################################
+
+# Challenge 6
+
+x = # Ask user for three numbers. Store them in x, y, and z
+y = 
+z = 
+
+# Use conditional statements to determine whether x, y, or z is the biggest
+
+
+# Use conditional statements to determine whether x, y, or z is the smallest
+
+
+# Print Results
+    
+    
+    
+    
+    
 
