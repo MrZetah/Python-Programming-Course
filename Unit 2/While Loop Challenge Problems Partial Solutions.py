8ce04679880c5398c2057f25e5b287298dba7d5e
MrZetah <154997315+MrZetah@users.noreply.github.com>
2024-05-02 11:42:29 -0500

    Update While Loop Challenge Problems Partial Solutions.py

--- Unit 2/While Loop Challenge Problems Partial Solutions.py
+++ Unit 2/While Loop Challenge Problems Partial Solutions.py
@@ -1,1 +1,52 @@
+# While Loop Challenge Problems - Partial Solutions
+
+#######################################################
+
+# Challenge 1
+word = input('Enter a word or phrase: ')
+num = int(input('How many times would you like it repeated? '))
+
+while num > 0:
+    print(word)
+    # Add a line here to complete the challenge
+
+#######################################################
+
+# Challenge 2
+number = # Ask user for number
+factorial = __    # What will you need to initally set this variable to?
+
+while number > __:   # Figure out what number goes here
+    factorial = _______________     # How are you going to calculate the factorial?
+    number -= 1
+print(factorial)
+
+
+#######################################################
+
+# Challenge 3
+number = int(input('Enter a number: '))
+value = 0
+
+while number > value:
+    # Complete the body of this while loop (two lines are needed)
+    
+    
+
+#######################################################
+
+# Challenge 4
+total = 0
+number = float(input('Enter a number: '))
+
+      # Add user's first number to total
+
+while True:
+    repeat = # Ask user for another number)
+    if repeat == 'stop':
+        print( )   # Print the total
+        break
+    else:
+        # Add a line here
+
 
